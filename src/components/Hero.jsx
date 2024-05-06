fdf125f845bdf09c994cc1ef696fc7a3a0aa2b4c
Gift <giftgabanakgosi@gmail.com>
2024-05-06 10:12:03 +0200

    Navbar+Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,28 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { heroVideo, smallHeroVideo } from "../utils";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Hero = () => {
   const [videoSrc, setVideoSrc] = useState(
     window.innerWidth < 760 ? smallHeroVideo : heroVideo
   );
+
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoSrcSet);
+
+    return () => {
+      window.removeEventListener("resize", handleVideoSrcSet);
+    };
+  }, []);
 
   useGSAP(() => {
     gsap.to(".hero-title", { opacity: 1, delay: 1.5 });
@@ -16,7 +32,13 @@ const Hero = () => {
       <div className="h-5/6 w-full flex-center flex-col">
         <p className="hero-title">iPhone 15 Pro</p>
         <div className="md:w-10/12 w-9/12">
-          <video autoPlay muted playsInline={true} key={videoSrc}>
+          <video
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+            className="pointer-events-none"
+          >
             <source src={videoSrc} type="video/mp4" />
           </video>
         </div>
