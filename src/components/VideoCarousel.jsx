8472fe3b265a706d4e24f8379b72e8ba63508814
Gift <giftgabanakgosi@gmail.com>
2024-05-09 10:26:31 +0200

    video Carousel update

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -2,6 +2,7 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { useRef, useState, useEffect } from "react";
 import { hightlightsSlides } from "../constants";
+import { pauseImg, playImg, replayImg } from "../utils";
 
 const VideoCarousel = () => {
   {
@@ -19,15 +20,38 @@ const VideoCarousel = () => {
     isPlaying: false,
   });
 
+  const [loadedData, setLoadedData] = useState([]);
+
   {
     /**Destructure our state variables so we don't have to keep referencing them with the .dot referencing operator */
   }
   const { isEnd, startPlay, videoId, isLastVideo, isPlaying } = video;
 
+  //This useEffect hook mainly deals with the playing of the video
+
+  useEffect(() => {
+    if (loadedData.length > 3) {
+      if (!isPlaying) {
+        videoRef.current[videoId].pause();
+      } else {
+        startPlay && videoRef.current[videoId].play();
+      }
+    }
+  }, [startPlay, isPlaying, videoId, loadedData]);
+
   useEffect(() => {
     //first figure out where are we in our video playing journey
     const currentProgress = 0;
     let span = videoSpanRef.current;
+
+    if (span[videoId]) {
+      //animate the progress of the video
+      let anim = gsap.to(span[videoId], {
+        //bare in mind we can also call methods as props to animate, its not limuted to props such as opacity,z,x,and y parameters
+        onUpdate: () => {},
+        onComplete: () => {},
+      });
+    }
   }, [videoId, startPlay]);
 
   {
@@ -41,11 +65,23 @@ const VideoCarousel = () => {
           <div key={list.id} id="slider" className="sm:pr-20 pr-10">
             <div className="video-carousel_container">
               <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
-                <video id="video" playsInline={true} preload="auto" muted>
+                <video
+                  id="video"
+                  playsInline={true}
+                  preload="auto"
+                  muted
+                  ref={(el) => (videoRef.current[index] = el)}
+                  onPlay={() => {
+                    setVideo((prevVideo) => ({
+                      ...prevVideo,
+                      isPlaying: true,
+                    }));
+                  }}
+                >
                   <source src={list.video} type="video/mp4" />
                 </video>
               </div>
-              {/**Sliding Carousel Button */}
+              {/**Sliding Carousel*/}
               <div className="absolute top-12 left-[5%] z-10">
                 {list.textLists.map((textArr) => (
                   <p key={textArr} className="md:text-xl text-xl font-medium">
@@ -56,6 +92,29 @@ const VideoCarousel = () => {
             </div>
           </div>
         ))}
+      </div>
+      {/**Sliding Carousel Button */}
+      <div className="relative flex-center mt-10">
+        <div className="flex-center py-5 px-7 bg-gray-300 rounded-full">
+          {videoRef.current.map((_, index) => (
+            <span
+              key={index}
+              ref={(el) => (videoDivRef.current[index] = el)}
+              className="h-3 w-3 bg-gray-200 mx-2 rounded-full relative cursor-pointer"
+            >
+              <span
+                className="absolute w-full h-full rounded-full"
+                ref={(el) => (videoSpanRef.current[index] = el)}
+              />
+            </span>
+          ))}
+        </div>
+        {/**Reset/Pause button next to carousel slider button */}
+        <button className="control-btn">
+          <img
+            src={isLastVideo ? replayImg : !isPlaying ? playImg : pauseImg}
+          />
+        </button>
       </div>
     </>
   );
