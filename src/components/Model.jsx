eda78d26086c2c0ac6548106e6c1712690b2de59
Gift <giftgabanakgosi@gmail.com>
2024-05-22 12:04:00 +0200

    error rectified, model object array of colors was named colors instead of color, model is able to render and no longer getting context lost error

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,9 +1,9 @@
 import { useGSAP } from "@gsap/react";
-
 import gsap from "gsap";
 import ModelView from "./ModelView";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { yellowImg } from "../utils";
+
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
@@ -13,7 +13,7 @@ const Model = () => {
   const [size, setSize] = useState("small");
   const [model, setModel] = useState({
     title: "iPhone 15 Pro in Natural Titanium",
-    colors: ["#8F8A81", "#FFE7B9", "#6F6C64"],
+    color: ["#8F8A81", "#FFE7B9", "#6F6C64"],
     img: yellowImg,
   });
 
@@ -29,74 +29,77 @@ const Model = () => {
   const [smallRotation, setSmallRotation] = useState(0);
   const [largeRotation, setLargeRotation] = useState(0);
 
+  useEffect(() => {}, [size]);
+
   useGSAP(() => {
-    gsap.to("heading", { y: 0, opacity: 1 });
+    gsap.to("#heading", { y: 0, opacity: 1 });
   }, []);
 
   return (
     <section className="common-padding">
-      <div className="screen-width-max">
+      <div className="screen-max-width">
         <h1 id="heading" className="section-heading">
-          Take a closer look
+          Take a closer look.
         </h1>
+
         <div className="flex flex-col items-center mt-5">
           <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
             <ModelView
               index={1}
-              gsapType="view1"
               groupRef={small}
+              gsapType="view1"
               controlRef={cameraControlSmall}
-              setRotationState={smallRotation}
+              setRotationState={setSmallRotation}
               item={model}
               size={size}
             />
+
             <ModelView
-              index={1}
-              gsapType="view2"
+              index={2}
               groupRef={large}
+              gsapType="view2"
               controlRef={cameraControlLarge}
-              setRotationState={largeRotation}
+              setRotationState={setLargeRotation}
               item={model}
               size={size}
             />
 
             <Canvas
-              className="w-full h-full "
+              className="w-full h-full"
               style={{
                 position: "fixed",
                 top: 0,
-                right: 0,
-                bottom: onabort,
+                bottom: 0,
                 left: 0,
+                right: 0,
+                overflow: "hidden",
               }}
               eventSource={document.getElementById("root")}
             >
-              {/**View port allows us to render different views of a model on the same canvas */}
               <View.Port />
             </Canvas>
           </div>
 
           <div className="mx-auto w-full">
             <p className="text-sm font-light text-center mb-5">{model.title}</p>
+
             <div className="flex-center">
-              {/**Model Colour selection */}
               <ul className="color-container">
-                {models.map((item, index) => (
+                {models.map((item, i) => (
                   <li
-                    className="rounded-full w-6 h-6 mx-2 cursor-pointer"
-                    key={index}
+                    key={i}
+                    className="w-6 h-6 rounded-full mx-2 cursor-pointer"
                     style={{ backgroundColor: item.color[0] }}
                     onClick={() => setModel(item)}
                   />
                 ))}
               </ul>
-              {/**Buttons to manage model sizes onClick */}
-              {/**The size state property will determine which size is selected, if selected size==value then make backround white and text color black */}
+
               <button className="size-btn-container">
                 {sizes.map(({ label, value }) => (
                   <span
+                    key={label}
                     className="size-btn"
-                    key={{ label }}
                     style={{
                       backgroundColor: size === value ? "white" : "transparent",
                       color: size === value ? "black" : "white",
