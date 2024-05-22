ee44e5771884af3de2a6cbc28feecb188846afdb
Gift <giftgabanakgosi@gmail.com>
2024-05-22 11:20:28 +0200

    update to modelviewer + scene glb configuration, getting context Lost rendering error, to fix

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -79,6 +79,7 @@ const Model = () => {
           <div className="mx-auto w-full">
             <p className="text-sm font-light text-center mb-5">{model.title}</p>
             <div className="flex-center">
+              {/**Model Colour selection */}
               <ul className="color-container">
                 {models.map((item, index) => (
                   <li
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,5 +1,15 @@
-import { View } from "@react-three/drei";
+import {
+  Html,
+  OrbitControls,
+  PerspectiveCamera,
+  View,
+} from "@react-three/drei";
+
 import * as THREE from "three";
+import Lights from "./Lights";
+import Loader from "./Loader";
+import IPhone from "./IPhone";
+import { Suspense } from "react";
 
 const ModelView = ({
   index,
@@ -7,20 +17,41 @@ const ModelView = ({
   gsapType,
   controlRef,
   setRotationState,
-  item,
   size,
+  item,
 }) => {
   return (
     <View
       index={index}
       id={gsapType}
-      className={`border-2 border-red-500 w-full h-full ${
-        index === 2 ? "right-[-100%]" : ""
-      }`}
+      className={`w-full h-full absolute ${index === 2 ? "right-[-100%]" : ""}`}
     >
-      {/**Ambient lighting */}
+      {/* Ambient Light */}
       <ambientLight intensity={0.3} />
-      <perspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      <Lights />
+
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      />
+
+      <group
+        ref={groupRef}
+        name={`${index === 1} ? 'small' : 'large`}
+        position={[0, 0, 0]}
+      >
+        <Suspense fallback={<Loader />}>
+          <IPhone />
+        </Suspense>
+      </group>
     </View>
   );
 };
